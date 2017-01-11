f712b9631af46608aa8ea5e8ea1df7c592685739
Xnsam <aksonsam@gmail.com>
2017-01-11 09:48:31 +0530

    Add files via upload

---
+++ DiceRoller.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import random
+
+
+def roll():
+    r = random.randint(1, 6)
+    s = str(r)
+    e.delete(0, END)
+    e.insert(0, s)
+
+
+root = Tk()
+root.geometry("200x200")
+
+label = Label(root, text="Dice Roller")
+e = Entry(root, width=10)
+button1 = Button(root, text="Click to roll the dice", fg="blue", command=roll)
+
+label.grid(row=0, sticky=N)
+e.grid(row=1, sticky=N)
+button1.grid(row=2, sticky=N)
+
+root.mainloop()
